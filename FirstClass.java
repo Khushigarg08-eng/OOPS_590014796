562c58957a6eaaa20b2f9533cef2b03685d70843
Khushigarg08-eng <khushigarg166@gmail.com>
2026-01-21 08:42:59 +0530

    Add files via upload

---
+++ FirstClass.java
@@ -0,0 +1,7 @@
+class FirstClass {
+    public static void main (String[] args) {
+        System.out.println("Hello World");
+        // we will use println here bcz we want our second line hello world in a different line or we can even use \n .
+        System.out.print("Hello World");
+    }
+}
