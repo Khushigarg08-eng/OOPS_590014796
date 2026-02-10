bb906e31bc5df2e152071ccc60b1e0d86bd05e22
Khushigarg08-eng <khushigarg166@gmail.com>
2026-02-10 15:50:06 +0530

    Add files via upload

---
+++ Code3.java
@@ -0,0 +1,14 @@
+public class Code3 {
+    public static void main(String[] args) {
+
+        // Storing command line arguments into a String array
+        String[] arr = args;
+
+        // Printing the array elements
+        System.out.println("Array elements are:");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
+
