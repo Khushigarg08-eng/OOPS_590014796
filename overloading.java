50f751840236917d9885860358f3b5b81e2a346f
Khushigarg08-eng <khushigarg166@gmail.com>
2026-02-13 19:01:21 +0530

    Add files via upload

---
+++ overloading.java
@@ -0,0 +1,17 @@
+class MathOperation {
+    int add(int a, int b) {
+        return a + b;
+    }
+
+    int add(int a, int b, int c) {
+        return a + b + c;
+    }
+}
+
+public class overloading {
+    public static void main(String[] args) {
+        MathOperation m = new MathOperation();
+        System.out.println(m.add(2, 3));      
+        System.out.println(m.add(2, 3, 4));   
+    }
+}
