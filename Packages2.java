f5bd08b9d875468636dd7301c29bbcbee7741d92
Khushigarg08-eng <khushigarg166@gmail.com>
2026-02-18 08:27:56 +0530

    Add files via upload

---
+++ Packages2.java
@@ -0,0 +1,9 @@
+package Packages2;
+import Package2.Main1;
+public class Packages2 {
+        public static void main(String[] args) {
+            Car1 p = new Car1();
+            System.out.println("mileage of the car" + p.mileage);   
+        }
+    }    
+
